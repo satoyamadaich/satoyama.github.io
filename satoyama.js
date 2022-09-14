5dbf6309f14b01d4bb0f17b81d2081c8bdd3984b
ympbyc <ympbyc@gmail.com>
2022-09-14 22:39:30 +0900

    form to send via smtp

--- satoyama.js
+++ satoyama.js
@@ -7,20 +7,60 @@ window.addEventListener("DOMContentLoaded", function () {
 		return acc;
 	    }, {});
     }
-    
+
     var ps = params(location.search);
 
-    var templateEl = document.getElementById("mail-template");
-    var mail_template_text = templateEl.innerHTML.replace("TITLE",decodeURI(ps.title)).replace("DATE", decodeURI(ps.date));
-    templateEl.innerHTML = mail_template_text;
-
     document.querySelectorAll(".date").forEach((el)=>el.innerHTML = decodeURI(ps.date));
+    document.querySelector("#in-date").value = decodeURI(ps.date);
     document.querySelectorAll(".title").forEach((el)=>el.innerHTML = decodeURI(ps.title));
 
-    document.querySelectorAll(".mailto-button").forEach((b)=>{
+    function make_row (label) {
+        let tr  = document.createElement("tr");
+        let td1 = document.createElement("th");
+        let td2 = document.createElement("td");
+        let input = document.getElementById(label.attributes.for.value);
+        td1.appendChild(label);
+        td2.appendChild(input);
+        tr.appendChild(td1); tr.appendChild(td2);
+        return tr;
+    }
+
+    let form = document.getElementById("signup-form");
+    let tbl = document.createElement("table");
+    form.appendChild(tbl);
+    form.querySelectorAll("label").forEach((lbl)=>{
+        tbl.appendChild(make_row(lbl));
+    });
+
+    form.addEventListener("submit", (e)=>{
+        e.preventDefault();
+        let text = decodeURI(new URLSearchParams(new FormData(form)).toString().split(/[\?\&]/).join("\n\n"));
+
+        let btn = form.querySelector("input[type=submit]");
+        btn.value = "送信中";
+
+        Email.send({
+            SecureToken : "0f3f703e-d0d9-44b2-a4f5-75bbe1a4954a",
+            To : 'satoyamadaich@gmail.com',
+            From : "satoyamadaich@gmail.com",
+            ReplyTo: document.getElementById("in-email").value,
+            Subject : "" + ps.date + " " + ps.title + " 参加申し込み",
+            Body : text
+        }).then(
+            message => {
+                alert(message);
+                btn.value = "送信済み";
+                btn.disabled= true;
+            }
+        );
+    });
+
+
+
+    /*document.querySelectorAll(".mailto-button").forEach((b)=>{
 	b.href = "mailto:satoyamadaich@gmail.com?subject=" +
 	    encodeURI("里山大地参加申込み") +
 	    "&body=" +
 	    encodeURI(mail_template_text);
-    });
+    });*/
 });
